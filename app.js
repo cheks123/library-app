0cc0e9ea63c81b18e632cef11eee9793134fdc2f
Chekwube Utomi <chekwubeutomi@gmail.com>
2025-05-03 05:05:13 +0100

    node_module removed

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+require('dotenv').config();
 
 var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://cooluser:testing123@cluster0.bb3fttp.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB = process.env.DATABASE_URI
 
 main().catch((err) => console.log(err));
 async function main() {
